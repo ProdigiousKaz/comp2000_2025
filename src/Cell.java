ad11942e23caffe05e27dd4559731b03810416f3
ProdigiousKaz <99377098+ProdigiousKaz@users.noreply.github.com>
2025-08-22 10:24:12 +1000

    task 6

--- src/Cell.java
+++ src/Cell.java
@@ -1,22 +1,23 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Cell {
+public class Cell extends Rectangle {
   // fields
-  int x;
-  int y;
   static int size = 35;
 
   // constructors
   public Cell(int inX, int inY) {
     x = inX;
     y = inY;
+    width = size;
+    size = width;
   }
 
   // methods
   public void paint(Graphics g, Point mousePos) {
-    if(contains(mousePos)) {
+    if(mousePos != null && contains(mousePos)) {
       g.setColor(Color.GRAY);
     } else {
       g.setColor(Color.WHITE);
@@ -26,11 +27,4 @@ public class Cell {
     g.drawRect(x, y, size, size);
   }
 
-  public boolean contains(Point p) {
-    if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-    } else {
-      return false;
-    }
-  }
 }
